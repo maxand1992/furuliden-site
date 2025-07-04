c506da14bca0e36e34cf1ee14043f43eb74b7a8c
maxand1992 <anderstedtmax@gmail.com>
2025-07-04 15:38:08 +0200

    new update

--- furuliden-site/blog-site/.eleventy.js
+++ furuliden-site/blog-site/.eleventy.js
@@ -1,23 +1,29 @@
 const { DateTime } = require("luxon");
 
-
 module.exports = function(eleventyConfig) {
+  // Slug-filter för att generera URL-vänliga filnamn
+  eleventyConfig.addFilter("slug", function(str) {
+    return str.toLowerCase()
+              .replace(/\s+/g, '-')
+              .replace(/[^\w\-]+/g, '')
+              .replace(/\-\-+/g, '-')
+              .replace(/^-+|-+$/g, '');
+  });
+
+  // Datum-filter
   eleventyConfig.addFilter("date", (dateObj, format = "yyyy-LL-dd") => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(format);
   });
 
-  // Kopiera statiska filer som ligger i "images", "styles", "favicon"
+  // Kopiera statiska filer
   eleventyConfig.addPassthroughCopy("favicon.png");
   eleventyConfig.addPassthroughCopy("logo.png");
-
 
   return {
     dir: {
       input: ".",              // Rotmapp
       includes: "_includes",   // Layouts, partials
-      output: "../blog" // Output efter build
-    
-
+      output: "../blog/"       // Output till root/blog/
     },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
